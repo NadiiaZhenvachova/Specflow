b92b917a99550e417a612d562a5faf38e5a47ae6
Nadiia Kozlovska <nadazenvacova@gmail.com>
2022-11-07 20:38:01 +0200

    Locators

---
+++ Locators/Xpath.cs
@@ -0,0 +1,20 @@
+namespace Locators;
+
+public class Xpath
+{
+    /*
+    Xpath Operators
+    ------------------------------------------------------------------------------------------
+    And - //input[@id = 'someID' and @class = 'someClass']
+    Or - //input[@id = 'someID' or @class = 'someClass']
+    Equals - //input[@id = 'someID']
+    
+    Xpath FUNCTIONS
+    ------------------------------------------------------------------------------------------
+    Contains - //h4[contains(@id,'user-name')]
+    Text - //h4[contains(text(),'usern')]
+    Last - //form//input[last()-1]
+    Starts-with - //h4[starts-with(text(),'Accep')]
+    Normalize-space - removes spaces before and after the string - normalize-space(@id)
+     */
+}
---
+++ Locators/Сss.cs
@@ -0,0 +1,48 @@
+namespace Locators;
+
+public class Сss
+{
+    /*
+     
+    CONSOLE SEARCH
+    ------------------------------------------------------------------------------
+    Xpath - $x("//input")
+    Css - $$("input")
+    
+    CSS LOCATOR
+    ------------------------------------------------------------------------------
+    Direct child:
+    Xpath - //form/input
+    Css - form>input
+    Normal next tag:
+    Xpath - //form//input
+    Css - form input
+    Attribute:
+    Xpath - "//form//input[@id='user-name']"
+    Css - form input[id='user-name']
+    
+    CSS LOCATOR SHORTHAND
+    ------------------------------------------------------------------------------
+    ID:
+    Xpath - //input[@id='user-name']
+    Css - input#user-name
+    Class:
+    Xpath - //div[@class='error-message-container']
+    Css - div.error-message-container
+    Attribute with operator:
+    Xpath - //input[@id='user-name' and  @name = 'user-name']
+    Css - input[id='user-name'][name = 'user-name']
+    
+    CSS LOCATOR FUNCTION
+    ------------------------------------------------------------------------------
+    nth-of-type:
+    Css - form input:th-of-type(1)
+    Prefix:
+    Css - input[id⌃='use'] 
+    Suffix:
+    Css - input[id$='use'] 
+    Substring:
+    Css - input[id*='login-'] 
+    
+    */
+}
